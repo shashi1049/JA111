9b950802dcb52f9475e099d36cce75eb41986652
shashi1049 <shashi290396@gmail.com>
2023-01-24 20:26:11 +0530

    welcome

---
+++ One/src/com/shashi/WelCome.java
@@ -0,0 +1,12 @@
+package com.shashi;
+
+public class WelCome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to java");
+
+	}
+
+}
