5435bd5eb695ec26337c6acbdc0c488ec1db0980
shashi1049 <shashi290396@gmail.com>
2023-01-25 23:22:55 +0530

    day1

---
+++ One/src/com/shashi/Switch1.java
@@ -0,0 +1,30 @@
+package com.shashi;
+
+public class Switch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+		switch(i)
+		{
+		case 5:
+		System.out.println("Five");
+		break;
+		case 10:
+		System.out.println("Ten");
+		break;
+		case 15:
+		System.out.println("Fifteen");
+		break;
+		case 20:
+		System.out.println("Twenty");
+		break;
+		default:
+		System.out.println("Default");
+		break;
+		}
+
+	}
+
+}
