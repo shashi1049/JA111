b145c1ce31f93a177715926fda96965eaca3b010
shashi1049 <shashi290396@gmail.com>
2023-01-25 23:21:44 +0530

    day1

---
+++ One/src/com/shashi/IfElse2.java
@@ -0,0 +1,27 @@
+package com.shashi;
+
+public class IfElse2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+		int j;
+		if(i==10)
+		{
+		j=20;
+		}
+		else if(i==20)
+		{
+		j=30;
+		}
+		else
+		{
+		j=40;
+		}
+		System.out.println(j);
+
+
+	}
+
+}
