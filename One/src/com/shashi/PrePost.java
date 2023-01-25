0194428087145c88d40359c22cf3f7eb6ac4e31d
shashi1049 <shashi290396@gmail.com>
2023-01-25 23:09:04 +0530

    day1

---
+++ One/src/com/shashi/PrePost.java
@@ -0,0 +1,19 @@
+package com.shashi;
+
+public class PrePost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		System.out.println(a); //10
+		System.out.println(a++);//10 //post-increment => (print, increase)
+		System.out.println(++a);//12
+		System.out.println(a--);//12
+		System.out.println(--a);//10
+		System.out.println(a);//10
+
+
+	}
+
+}
---
+++ One/src/com/shashi/PrePostSum.java
@@ -0,0 +1,14 @@
+package com.shashi;
+
+public class PrePostSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5;
+		System.out.println(++a - ++a);
+
+
+	}
+
+}
