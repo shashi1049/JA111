7ee2c605e755862ee236bb69831de3e93ccc0cfe
shashi1049 <shashi290396@gmail.com>
2023-01-26 01:07:18 +0530

    day1

---
+++ One/src/com/shashi/MyDetails.java
@@ -0,0 +1,13 @@
+package com.shashi;
+
+public class MyDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Shashi");
+		System.out.println("Lives in INDIA");
+
+	}
+
+}
