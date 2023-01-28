da69e3936b62e7e3de6db6477cbce10bfc7d7775
shashi1049 <shashi290396@gmail.com>
2023-01-28 22:43:00 +0530

    day1

--- One/src/com/shashi/Masai.java
+++ One/src/com/shashi/Masai.java
@@ -5,6 +5,7 @@ public class Masai {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Masai School");
+		System.out.println("A Tranformation In Education!");
 
 	}
 
