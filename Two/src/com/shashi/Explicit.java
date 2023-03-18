4b27a25f6e7512d031a3189da75ea4f1ca28d8f8
shashi1049 <shashi290396@gmail.com>
2023-03-18 11:50:01 +0530

    impl

---
+++ Two/src/com/shashi/Explicit.java
@@ -0,0 +1,15 @@
+package com.shashi;
+
+public class Explicit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+		byte b = (byte)i;
+		System.out.println(i+" "+b);
+
+
+	}
+
+}
--- Two/src/com/shashi/ImplicitError.java
+++ Two/src/com/shashi/ImplicitError.java
@@ -6,8 +6,8 @@ public class ImplicitError {
 		// TODO Auto-generated method stub
 		byte b1=60;
 		byte b2=70;
-		byte b=b1+b2;
-		System.out.println(b);
+		//byte b=b1+b2;
+		//System.out.println(b);
 		//Status: Compilation Error, Possible loss of precision.
 
 	}
