e82e371f5661e002f840cf9fab796acdefc8b945
shashi1049 <shashi290396@gmail.com>
2023-03-18 11:51:29 +0530

    impl

---
+++ Two/src/com/shashi/Explicit2.java
@@ -0,0 +1,15 @@
+package com.shashi;
+
+public class Explicit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte b1=30;
+		byte b2=30;
+		byte b=(byte)(b1+b2);
+		System.out.println(b);
+
+	}
+
+}
