dab48c9aa7fe25985a52fa7e35799d1388fec105
shashi1049 <shashi290396@gmail.com>
2023-03-18 11:46:30 +0530

    impl

---
+++ Two/src/com/shashi/ImplicitError.java
@@ -0,0 +1,15 @@
+package com.shashi;
+
+public class ImplicitError {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b1=60;
+		byte b2=70;
+		byte b=b1+b2;
+		System.out.println(b);
+		//Status: Compilation Error, Possible loss of precision.
+
+	}
+
+}
