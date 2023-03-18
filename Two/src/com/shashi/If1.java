cd48c9f2c908fa98ba8542230e6cb882a739a10c
shashi1049 <shashi290396@gmail.com>
2023-03-18 11:57:52 +0530

    If

---
+++ Two/src/com/shashi/If1.java
@@ -0,0 +1,22 @@
+package com.shashi;
+
+public class If1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		int j;
+		if(i==10)
+		{
+		j=20;
+		}
+		else
+		{
+		j=30;
+		}
+		System.out.println(j);
+
+
+	}
+
+}
