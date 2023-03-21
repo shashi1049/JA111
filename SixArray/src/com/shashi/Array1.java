26c52a4cf64764a993093d9db803d43e26a32ae9
shashi1049 <shashi290396@gmail.com>
2023-03-21 23:18:10 +0530

    a

---
+++ SixArray/src/com/shashi/Array1.java
@@ -0,0 +1,18 @@
+package com.shashi;
+
+public class Array1 {
+	
+	public static void main(String[] args) {
+		// create an array
+		int[] marks = {40, 40, 55, 25, 55};
+		// access each array elements
+		System.out.println("Accessing Elements of Array:");
+		System.out.println("First Element: " + marks[0]);
+		System.out.println("Second Element: " + marks[1]);
+		System.out.println("Third Element: " + marks[2]);
+		System.out.println("Fourth Element: " + marks[3]);
+		System.out.println("Fifth Element: " + marks[4]);
+		}
+
+
+}
