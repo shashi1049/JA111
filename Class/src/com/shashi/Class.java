c8ee66b70d365b14d0c92d2ffee95e5c52eece30
shashi1049 <shashi290396@gmail.com>
2023-07-26 21:47:00 +0530

    Class

---
+++ Class/src/com/shashi/Class.java
@@ -0,0 +1,22 @@
+package com.shashi;
+
+
+public class Class{
+	int x=10; //instance variable
+	public static void main(String[] args){
+		int y=10; //local variable of main mehtod
+		System.out.println(y); // 10
+		//System.out.println(x);//error
+}
+}
+
+class Main{
+	int x=10;
+	public static void main(String[] args){
+		int y=10;
+		System.out.println(y);
+		Main obj=new Main();// after creating obj of class we can use non static variable.
+		System.out.println(obj.x);
+}
+}
+
