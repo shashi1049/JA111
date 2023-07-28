95b8f7359dd70de9e3b8a4ff45a5e77b8f6beaa8
shashi1049 <shashi290396@gmail.com>
2023-07-28 12:40:30 +0530

    Relational operator

---
+++ Operators/src/com/shashi/Relational.java
@@ -0,0 +1,15 @@
+package com.shashi;
+
+public class Relational {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10 < 20);
+		System.out.println('a' < 10);
+		System.out.println('a' < 96.7);
+		System.out.println('a' >'A');
+		//System.out.println(true < false);// CE: Relational operator can not be applied on boolean value.
+		//System.out.println(10<20>40); // Nesting is not allowed.
+	}
+
+}
