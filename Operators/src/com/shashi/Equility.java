f2136896c26e456bd6a95173aa1d785f907bc6a6
shashi1049 <shashi290396@gmail.com>
2023-07-28 12:47:00 +0530

    Equility

---
+++ Operators/src/com/shashi/Equility.java
@@ -0,0 +1,16 @@
+package com.shashi;
+
+public class Equility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Equility operators (== and !=)
+		System.out.println(10 == 20);
+		System.out.println('a' == 'b');
+		System.out.println('a'== 97.0);
+		System.out.println(false == false);
+
+	}
+
+}
