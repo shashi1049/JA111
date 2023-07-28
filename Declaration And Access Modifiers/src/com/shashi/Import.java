75a9dacab36306ea73ec1cb81f22d6adc63758ba
shashi1049 <shashi290396@gmail.com>
2023-07-28 23:09:09 +0530

    import

---
+++ Declaration And Access Modifiers/src/com/shashi/Import.java
@@ -0,0 +1,21 @@
+package com.shashi;
+
+import java.util.ArrayList;
+
+public class Import {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Fully Qualified name
+		
+		java.util.ArrayList l = new java.util.ArrayList<>();
+		
+		
+		
+		// By importing
+		
+		ArrayList<Integer> l1 = new ArrayList<>();
+	}
+
+}
---
+++ Declaration And Access Modifiers/src/com/shashi/Import2.java
@@ -0,0 +1,13 @@
+package com.shashi;
+
+import java.util.Date;
+import java.sql.*;
+public class Import2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d = new Date();
+		System.out.println(d.getClass().getName());
+	}
+
+}
