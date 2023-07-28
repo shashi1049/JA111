5fd3a821274906ad3ee1ec1f68e6952e6cfcda92
shashi1049 <shashi290396@gmail.com>
2023-07-28 13:17:19 +0530

    Conditional

---
+++ Operators/src/com/shashi/Conditional.java
@@ -0,0 +1,14 @@
+package com.shashi;
+
+public class Conditional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Only possiable terniary operator in java (?:)
+		
+		int x = (10< 20)? 30 : 40;
+		System.out.println(x);//30
+
+	}
+
+}
