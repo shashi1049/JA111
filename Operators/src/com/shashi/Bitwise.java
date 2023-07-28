bd45e214ba1c1bbd627be2f4f75b966f72d4b0fc
shashi1049 <shashi290396@gmail.com>
2023-07-28 13:03:06 +0530

    Bitwise

---
+++ Operators/src/com/shashi/Bitwise.java
@@ -0,0 +1,19 @@
+package com.shashi;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(true & false);
+		System.out.println(true | false);
+		System.out.println(true ^ false);
+		// We can apply bitwise operator on integral types also.
+		System.out.println(4 & 5);
+		System.out.println(4 | 5);
+		System.out.println(4 ^ 5);
+		
+		
+
+	}
+
+}
