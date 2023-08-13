3abf9fcda3454f9572a5525b228a3c7c8005ef6a
shashi1049 <shashi290396@gmail.com>
2023-08-13 23:17:26 +0530

    Sleep

---
+++ MultiThreading/src/com/shashi/Sleep2.java
@@ -0,0 +1,20 @@
+package com.shashi;
+
+public class Sleep2 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		for(int i=0;i<10;i++) {
+			System.out.println("Slide :"+ i);
+			Thread.sleep(5000);
+		
+		}
+		for(int i=0;i<10;i++) {
+			System.out.println("Slide :"+ i);
+			Thread.sleep(2000);
+		
+		}
+
+	}
+
+}
