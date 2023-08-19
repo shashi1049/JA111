79fb564a46a02953e772c14f4bb0f717572c29c5
shashi1049 <shashi290396@gmail.com>
2023-08-19 21:45:15 +0530

    SelectionSortSort

---
+++ Sorting Technique/src/SelectionSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+	
+	public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        selectionSort(arr);
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+}
