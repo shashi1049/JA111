2afecf33ffe97402eac0621c8ffb26fa3804e360
shashi1049 <shashi290396@gmail.com>
2023-08-19 21:30:18 +0530

    Comparator

---
+++ CustomComparator/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+	
+	public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Alice", 85));
+        students.add(new Student("Bob", 92));
+        students.add(new Student("Charlie", 78));
+        students.add(new Student("David", 95));
+        students.add(new Student("Eve", 88));
+
+        // Sorting the students based on their marks using a custom comparator
+        Collections.sort(students, new Comparator<Student>() {
+            @Override
+            public int compare(Student student1, Student student2) {
+                return Integer.compare(student2.getMarks(), student1.getMarks()); // Sorting in descending order
+            }
+        });
+
+        // Printing the sorted list of students
+        for (Student student : students) {
+            System.out.println(student);
+        }
+	}
+
+}
---
+++ CustomComparator/src/Student.java
@@ -0,0 +1,28 @@
+
+public class Student {
+	
+	private String name;
+    private int marks;
+
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
+}
