6d246931ce05e88d8f42a8ae1ecb7746b4c98063
shashi1049 <shashi290396@gmail.com>
2023-08-23 23:08:36 +0530

    DSA

---
+++ DSA/src/PalindromeChecker.java
@@ -0,0 +1,30 @@
+
+public class PalindromeChecker {
+	
+	public static void main(String[] args) {
+        String input = "radar"; // Replace with the string you want to check
+        boolean isPalindrome = checkPalindrome(input);
+
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    public static boolean checkPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+}
