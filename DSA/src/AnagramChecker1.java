32af8c387e71d50ba989680ff6c283d81ea806c7
shashi1049 <shashi290396@gmail.com>
2023-08-26 23:23:33 +0530

    DSA

---
+++ DSA/src/AnagramChecker1.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class AnagramChecker1 {
+	
+	public static void main(String[] args) {
+        String str1 = "listen"; // Replace with the first string
+        String str2 = "silent"; // Replace with the second string
+
+        boolean areAnagrams = checkAnagrams(str1, str2);
+
+        if (areAnagrams) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+
+    public static boolean checkAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+}
