7a529dc1cbda43d4d53e9bbad8bb4642d5082663
shashi1049 <shashi290396@gmail.com>
2023-09-13 23:13:19 +0530

    final

---
+++ Collection/src/EvaluateReversePolishNotation.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+	
+	public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (token.matches("-?\\d+")) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+}
